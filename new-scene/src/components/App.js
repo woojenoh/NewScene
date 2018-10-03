a42b14345746dd6a6ce6777f85169d28036929de
WOOJE <woojenoh@gmail.com>
2018-10-03 17:14:03 +0900

    FeedPhoto
    FeedPhoto 컴포넌트를 생성하고, App에서 JSON을 만든 뒤 불러왔습니다.

--- new-scene/src/components/App.js
+++ new-scene/src/components/App.js
@@ -1,11 +1,69 @@
 import React, { Component } from "react";
-import Navigation from "./Navigation/index";
+import Navigation from "./Navigation";
+import FeedPhoto from "./FeedPhoto";
 
 class App extends Component {
+  state = {
+    posts: [
+      {
+        id: "1",
+        username: "JIEUN",
+        profilePhoto:
+          "http://www.kyeongin.com/mnt/file/201807/20180721000818478_1.jpg",
+        movieId: "1",
+        photo:
+          "https://scontent-atl3-1.cdninstagram.com/vp/fc550259ae8484d90feddd6ee019f2cd/5BC5968E/t51.2885-15/e35/17881542_723982161114288_5150746053783322624_n.jpg",
+        message: "멋지네요!",
+        like: "1"
+      },
+      {
+        id: "2",
+        username: "HYUNJOO",
+        profilePhoto:
+          "http://www.sporbiz.co.kr/news/photo/201804/222816_180822_5132.jpg",
+        movieId: "2",
+        photo: "http://www.itdaily.kr/news/photo/201503/61286_67165_3835.jpg",
+        message: "지금은 공사 중이에요 ㅜㅜ",
+        like: "5"
+      }
+    ],
+    movies: [
+      {
+        id: "1",
+        title: "도둑들",
+        location: "대전 동구 대학로 62"
+      },
+      {
+        id: "2",
+        title: "더 킹",
+        location: "대전광역시 유성구 대학로 99"
+      }
+    ]
+  };
+
   render() {
+    const { posts, movies } = this.state;
+
     return (
       <div className="App">
         <Navigation />
+        <main className="main">
+          <section className="feed">
+            {posts.map(post => {
+              const movie = movies.filter(movie => {
+                return post.movieId === movie.id;
+              });
+              return (
+                <FeedPhoto
+                  key={post.id}
+                  {...post}
+                  title={movie[0].title}
+                  location={movie[0].location}
+                />
+              );
+            })}
+          </section>
+        </main>
       </div>
     );
   }
---
+++ new-scene/src/components/FeedPhoto/presenter.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Ionicon from "react-ionicons";
+
+import "./styles.css";
+
+class presenter extends Component {
+  render() {
+    const {
+      username,
+      profilePhoto,
+      photo,
+      message,
+      like,
+      title,
+      location
+    } = this.props;
+
+    return (
+      <div className="feed__card">
+        <div className="card__header">
+          <img src={profilePhoto} alt="1" className="card__header-img" />
+          <span className="card__header-name">{username}</span>
+          <span className="card__close">×</span>
+        </div>
+        <img src={photo} alt="1" className="card__img" />
+        <div className="card__footer">
+          <div className="footer__rating">
+            <div className="rating__up">
+              <Ionicon icon="md-thumbs-up" fontSize="25px" />
+              <span>{like}</span>
+            </div>
+          </div>
+          <div className="footer__movie">
+            <span>
+              {title}({location})
+            </span>
+          </div>
+          <div className="footer__message">
+            <span>{message}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default presenter;
